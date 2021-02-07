787a88cb7e6bbdbbfabae0a970bc55944a087a25
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-07 15:13:35 +0400

    Add Layout

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>WebDev News | About</title>
+        <meta name='keywords' content='web development, programming' />
+      </Head>
+
+      <h1>About page</h1>
+    </div>
+  );
+};
+
+export default about;
