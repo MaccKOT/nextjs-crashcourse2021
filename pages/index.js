b52bff1cf67cc0c0de5188c5ce5312d71808833c
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-07 16:07:19 +0400

    Add Article components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 
-// import styles from '../styles/Home.module.css'; //local css import for module
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export default function Home({ articles }) {
+  // console.log(articles);
+
   return (
     <div>
       <Head>
@@ -10,7 +12,20 @@ export default function Home() {
         <meta name='keywords' content='web development, programming' />
       </Head>
 
-      <h1>Hello Next</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
